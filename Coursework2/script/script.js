5e7ae8da9cf8da103d3672d06f8c4234d7af26f8
dumitru-stratulat <62725538+dumitru-stratulat@users.noreply.github.com>
2022-01-20 14:44:54 +0000

    Add files via upload

---
+++ Coursework2/script/script.js
@@ -0,0 +1,16 @@
+
+function openLogin() {
+    document.getElementById("loginPopup").style.display = "block";
+  }
+
+  function closeLogin() {
+    document.getElementById("loginPopup").style.display = "none";
+  }
+
+  function openReg() {
+    document.getElementById("regPopup").style.display = "block";
+  }
+
+  function closeReg() {
+    document.getElementById("regPopup").style.display = "none";
+  }
